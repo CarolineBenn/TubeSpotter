1d11a8f4a983bbc3008a20111e7f02dd6b634758
CarolineBenn <lineybenn@gmail.com>
2016-04-24 12:50:00 +0100

    Have added new/create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,14 +12,39 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
     @train = Train.all
+    @line = ["Bakerloo", "Central", "Circle", "District", "Hammersmith & City", "Jubilee", "Metropolitan", "Northern", "Piccadilly", "Victoria", "Waterloo & City"]
   end
 
-  def create
 
+=begin
+ @beer = Beer.new(beer_params)
+   if @beer.save
+
+     @review = @beer.reviews.new(review_params)
+     @review.user = current_user
+     @review.pub_id = session[:current_pub_id]
+     @review.save
+     redirect_to @beer
+   else
+     redirect_to new_beer_path
+   end 
+=end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      @post.user = current_user
+      @post.save 
+      redirect_to @post
+    else
+      redirect_to new_post_path
+    end
   end
 
   def edit
-    
+    @post = Post.find(params[:id])
+    @train = Train.all
+    @line = ["Bakerloo", "Central", "Circle", "District", "Hammersmith & City", "Jubilee", "Metropolitan", "Northern", "Piccadilly", "Victoria", "Waterloo & City"]
   end
 
   def update
