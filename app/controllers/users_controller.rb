82bbac9865318efab868a4614cb75ae89ccd0286
CarolineBenn <lineybenn@gmail.com>
2016-04-25 21:04:44 +0100

    Added user edit form and routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,11 +18,25 @@ end
   end
 
   def edit
+    @user = current_user
   end
 
   def update
+    @user = current_user  
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   def delete
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:username, :first_name, :last_name, :photo, :location, :points, :gender)  
+    end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,8 @@ Rails.application.routes.draw do
   resources :posts
   resources :users
   resources :trains
+
+  get 'profile/edit', to: 'users#edit', as: 'edit_profile'
+
+  
 end
